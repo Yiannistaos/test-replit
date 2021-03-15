65ef2c054a29b3279f4c4eb29d891006f2cbe25f
Yiannis Christodoulou <yiannis@web357.com>
2021-03-15 00:32:39 +0000

    Add heading, footer and some styling

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <html>
 <head>
-	<title>PHP Test</title>
+<title>PHP Test Replit with Github</title>
 </head>
 <body>
+<h1>Replit tests</h1>
 <?php 
 echo '<p>Hello World.</p>'; 
-?> 
+?>
+
+<footer style="color:#999;">
+<p>Yiannis Christodoulou</p>
+</footer>
 </body>
 </html>
